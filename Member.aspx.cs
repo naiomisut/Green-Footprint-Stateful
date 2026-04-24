8f8777bc0589369528d3b7a3a9501944822c296e
Naiomi Sutphen <149907005+naiomisut@users.noreply.github.com>
2026-04-24 09:21:28 -0700

    Code for member aspx .cs

---
+++ Member.aspx.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Web.Security;
+using System.Xml;
+namespace YourNamespace
+{ 
+    public partial class Member : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("Login.aspx");
+            }
+            else
+            {
+                lblWelcome.Text = "Welcome, " + User.Identity.Name + "!";
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            Session.Clear();
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
